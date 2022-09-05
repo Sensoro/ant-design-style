4e70eb45e98d8055393e2d613d8a9cb3ee40e960
zhangfuhao <>
2022-09-05 16:33:59 +0800

    feat: complete two component affix、alert

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './index.js',
+  mode: "development",
+  entry: "./index.js",
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'css-loader',
-        ],
-      }, {
+        use: ["css-loader"],
+      },
+      {
         test: /\.less$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader', // 配合 autoprefixer +  browserslist 使用，相关配置写在 package.json
-          'less-loader',
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader", // 配合 autoprefixer +  browserslist 使用，相关配置写在 package.json
+          {
+            loader: "less-loader",
+            options: { lessOptions: { javascriptEnabled: true } },
+          },
         ],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'tdesign.css',
+      filename: "antd.css",
     }),
   ],
   devServer: {
-    contentBase: './',
-    host: '0.0.0.0',
+    contentBase: "./",
+    host: "0.0.0.0",
     compress: true,
     port: 9000,
     open: false,
     writeToDisk: true,
-    stats: 'errors-only',
+    stats: "errors-only",
   },
 };
