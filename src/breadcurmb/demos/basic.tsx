c76e5b545445941b9b4e79c5da541ef1047d30b1
zhangfuhao <>
2022-09-07 16:05:58 +0800

    feat: move breadbrumb doc

---
+++ src/breadcurmb/demos/basic.tsx
@@ -0,0 +1,33 @@
+import "../index.less";
+import "../_docs.less";
+
+export default () => {
+  return (
+    <>
+      <nav className="ant-breadcrumb">
+        <ol>
+          <li>
+            <span className="ant-breadcrumb-link">Home</span>
+            <span className="ant-breadcrumb-separator">/</span>
+          </li>
+          <li>
+            <span className="ant-breadcrumb-link">
+              <a href="">Application Center</a>
+            </span>
+            <span className="ant-breadcrumb-separator">/</span>
+          </li>
+          <li>
+            <span className="ant-breadcrumb-link">
+              <a href="">Application List</a>
+            </span>
+            <span className="ant-breadcrumb-separator">/</span>
+          </li>
+          <li>
+            <span className="ant-breadcrumb-link">An Application</span>
+            <span className="ant-breadcrumb-separator">/</span>
+          </li>
+        </ol>
+      </nav>
+    </>
+  );
+};
---
+++ src/breadcurmb/index.md
@@ -0,0 +1,21 @@
+---
+title: Breadcrumb 面包屑
+order: 0
+toc: content
+group:
+  title: 导航
+  order: 0
+nav:
+  title: 组件
+  path: /components
+---
+
+# Breadcrumb面包屑
+
+## 代码演示
+
+### 警告提示
+
+<code src="./demos/basic.tsx" />
+
+
