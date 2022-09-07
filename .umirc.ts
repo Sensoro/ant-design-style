3a32cf199d5d32a6228b0aa7222dda9fc1c8daf8
wangxingkang <wang_xingkang@qq.com>
2022-09-07 17:01:03 +0800

    docs: 完善文档

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ const logo = 'https://cdn.jsdelivr.net/gh/wangxingkang/pictures@latest/imgs/sens
 
 export default defineConfig({
   mode: 'site',
-  title: 'Sensoro Design Styles',
+  title: 'Sensoro Design',
   dynamicImport: {},
   favicon: logo,
   logo,
