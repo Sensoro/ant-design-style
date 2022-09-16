b6af45b0d939ae9fb3b5b7fa1ea78d6531630bc3
zhangfuhao <>
2022-09-16 18:07:39 +0800

    feat: add slider

---
+++ src/slider/demos/basic.tsx
@@ -0,0 +1,32 @@
+import { Slider, Switch, Space } from "antd";
+import { useState } from "react";
+import type { FC } from "react";
+import type { SliderMarks } from "antd/es/slider";
+
+const marks: SliderMarks = {
+  0: "0°C",
+  26: "26°C",
+  37: "37°C",
+  100: "100°C",
+};
+
+const App: FC = () => {
+  const [disabled, setDisabled] = useState(false);
+
+  const onChange = (checked: boolean) => {
+    setDisabled(checked);
+  };
+
+  return (
+    <Space style={{ width: "100%" }} direction="vertical" size={16}>
+      <div>
+        disabled: <Switch checked={disabled} onChange={onChange} />
+      </div>
+      <Slider defaultValue={30} disabled={disabled} />
+      <Slider range defaultValue={[20, 50]} disabled={disabled} />
+      <Slider marks={marks} defaultValue={37} disabled={disabled} />
+    </Space>
+  );
+};
+
+export default App;
---
+++ src/slider/index.md
@@ -0,0 +1,22 @@
+---
+title: Slider 滑动选择器
+order: 0
+toc: content
+group:
+  title: 数据录入
+  order: 0
+nav:
+  title: 组件
+  path: /components
+---
+
+# Slider 滑动选择器
+
+## 代码演示
+
+### 基本用法
+
+<code src="./demos/basic.tsx" />
+
+
+
