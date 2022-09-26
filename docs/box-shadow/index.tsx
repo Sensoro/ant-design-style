fb2265cfd90e3f6cae310add689d679d21bd4eae
zhangfuhao <>
2022-09-26 18:57:20 +0800

    feat: update box-shadow demo

--- docs/box-shadow/index.tsx
+++ docs/box-shadow/index.tsx
@@ -13,37 +13,92 @@ export default () => (
     </Col>
     <Col span={8}>
       <div className="shadow shadow-1">
-        <span>@sensoro-shadow-1</span>
-        <br />
-        卡片背景1
+        <div>
+          <span className="shadow-name">@sensoro-shadow-1</span>
+          <br />
+          卡片背景1
+        </div>
+        <div>
+          <span className="shadow-value">
+            x，y，blur ( 0，0，1 ) #3C4961，0.08
+          </span>
+          <br />
+          <span className="shadow-value">
+            x，y，blur ( 0，1，2 ) #6D7789，0.16
+          </span>
+        </div>
       </div>
     </Col>
     <Col span={8}>
       <div className="shadow shadow-2">
-        <span>@sensoro-shadow-2</span>
-        <br />
-        卡片背景2
+        <div>
+          <span className="shadow-name">@sensoro-shadow-2</span>
+          <br />
+          卡片背景2
+        </div>
+        <div>
+          <span className="shadow-value">
+            x，y，blur ( 0，0，2 ) #3C4961，0.08
+          </span>
+          <br />
+          <span className="shadow-value">
+            x，y，blur ( 0，4，8 ) #6D7789，0.16
+          </span>
+        </div>
       </div>
     </Col>
     <Col span={8}>
       <div className="shadow shadow-3">
-        <span>@sensoro-shadow-3</span>
-        <br />
-        时间选择器/级联选择/表单选择
+        <div>
+          <span className="shadow-name">@sensoro-shadow-3</span>
+          <br />
+          时间选择器/级联选择/表单选择
+        </div>
+        <div>
+          <span className="shadow-value">
+            x，y，blur ( 0，2，4 ) #3C4961，0.04
+          </span>
+          <br />
+          <span className="shadow-value">
+            x，y，blur ( 0，8，16 ) #6D7789，0.16
+          </span>
+        </div>
       </div>
     </Col>
     <Col span={8}>
       <div className="shadow shadow-4">
-        <span>@sensoro-shadow-4</span>
-        <br />
-        卡片hover
+        <div>
+          <span className="shadow-name">@sensoro-shadow-4</span>
+          <br />
+          卡片hover
+        </div>
+        <div>
+          <span className="shadow-value">
+            x，y，blur ( 0，2，8 ) #3C4961，0.04
+          </span>
+          <br />
+          <span className="shadow-value">
+            x，y，blur ( 0，16，24 ) #6D7789，0.16
+          </span>
+        </div>
       </div>
     </Col>
     <Col span={8}>
       <div className="shadow shadow-5">
-        <span>@sensoro-shadow-5</span>
-        <br />
-        弹窗
+        <div>
+          <span className="shadow-name">@sensoro-shadow-5</span>
+          <br />
+          弹窗
+        </div>
+        <div>
+          <span className="shadow-value">
+            x，y，blur ( 0，2，8 ) #3C4961，0.08{" "}
+          </span>
+          <br />
+          <span className="shadow-value">
+            x，y，blur ( 0，20，32 ) #6D7789，0.24{" "}
+          </span>
+        </div>
       </div>
     </Col>
   </Row>
