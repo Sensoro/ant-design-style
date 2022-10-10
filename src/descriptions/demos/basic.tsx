75e6874f2e67d7d35b98c9052a739ef82e5e57f8
zhangfuhao <>
2022-10-10 18:00:37 +0800

    feat: add descriptions demo

--- src/descriptions/demos/basic.tsx
+++ src/descriptions/demos/basic.tsx
@@ -1,8 +1,8 @@
-import { Descriptions } from "antd";
+import { Descriptions, Space } from "antd";
 import LocationOutlined from "@sensoro-design/icons/LocationOutlined";
 
 export default () => (
-  <>
+  <Space size={32}>
     <Descriptions title="基本信息" size="middle" column={1} colon={false}>
       <Descriptions.Item label="UserName">张福浩</Descriptions.Item>
       <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
@@ -12,5 +12,26 @@ export default () => (
         利星行D座
       </Descriptions.Item>
     </Descriptions>
-  </>
+
+    <Descriptions title="基本信息" size="middle" column={1} colon={false}>
+      <Descriptions.Item label="UserName" labelStyle={{ width: 84 }}>
+        张福浩
+      </Descriptions.Item>
+      <Descriptions.Item labelStyle={{ width: 84 }} label="Telephone">
+        184800000
+      </Descriptions.Item>
+      <Descriptions.Item labelStyle={{ width: 84 }} label="Live">
+        北京
+      </Descriptions.Item>
+      <Descriptions.Item labelStyle={{ width: 84 }} label="Remark">
+        empty
+      </Descriptions.Item>
+      <Descriptions.Item
+        labelStyle={{ width: 84 }}
+        label={<LocationOutlined style={{ marginTop: 4 }} />}
+      >
+        利星行D座
+      </Descriptions.Item>
+    </Descriptions>
+  </Space>
 );
