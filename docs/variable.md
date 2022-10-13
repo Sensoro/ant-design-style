0e5166e6df3095c27f2824454c4cb67b34708578
zhangfuhao <>
2022-10-13 15:52:20 +0800

    fix: button click box-shadow

---
+++ docs/variable.md
@@ -0,0 +1,15 @@
+---
+title: 变量
+order: 7
+group:
+  path: /
+nav:
+  title: 设计
+  path: /docs
+---
+
+## variable 变量
+
+**实际开发中请使用变量赋值**
+
+<code src="./variable/index.tsx" />
---
+++ docs/variable/index.tsx
@@ -0,0 +1,67 @@
+import "./index.less";
+import { Space } from "antd";
+import CopyOutlined from "@sensoro-design/icons/CopyOutlined";
+import { onCopy } from "../colors";
+
+const variables = [
+  {
+    name: "margin",
+    label: "内边距",
+    desc: "4 倍原则",
+    values: [
+      {
+        name: "lg",
+        value: 24,
+      },
+      {
+        name: "md",
+        value: 16,
+      },
+      {
+        name: "sm",
+        value: 12,
+      },
+      {
+        name: "xs",
+        value: 8,
+      },
+      {
+        name: "xss",
+        value: 4,
+      },
+    ],
+  },
+];
+
+export default () => {
+  return (
+    <div className="styles-variable">
+      {variables.map((item) => {
+        const { name, label, desc, values } = item;
+        return (
+          <div>
+            <h3>
+              {name} - {label}
+            </h3>
+            <ul className="demo">
+              {values.map((_) => {
+                return (
+                  <li>
+                    <span>
+                      @{name}-{_.name}: {_.value}px
+                    </span>
+
+                    <CopyOutlined
+                      className="copy-icon"
+                      onClick={() => onCopy(`@${name}-${_.name}`)}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
