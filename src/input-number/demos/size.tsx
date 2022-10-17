a59124c16a2f4fdb541c106f5668f01148bb6661
zhangfuhao <>
2022-10-17 14:14:47 +0800

    fix: add input-number size demo

--- src/input-number/demos/size.tsx
+++ src/input-number/demos/size.tsx
@@ -2,18 +2,46 @@ import { Space, InputNumber } from "antd";
 
 export default () => {
   return (
-    <Space direction="vertical">
-      <Space>
-        大
-        <InputNumber size="large" style={{ width: 160 }} defaultValue={100} />
+    <Space>
+      <Space direction="vertical">
+        <Space>
+          大
+          <InputNumber size="large" style={{ width: 240 }} defaultValue={100} />
+        </Space>
+        <Space>
+          中
+          <InputNumber style={{ width: 240 }} defaultValue={100} />
+        </Space>
+        <Space>
+          小
+          <InputNumber size="small" style={{ width: 240 }} defaultValue={0} />
+        </Space>
       </Space>
-      <Space>
-        中
-        <InputNumber style={{ width: 160 }} defaultValue={100} />
-      </Space>
-      <Space>
-        小
-        <InputNumber size="small" style={{ width: 160 }} defaultValue={0} />
+
+      <Space direction="vertical">
+        <Space>
+          <InputNumber
+            size="large"
+            style={{ width: 240 }}
+            defaultValue={100}
+            addonAfter="mA"
+          />
+        </Space>
+        <Space>
+          <InputNumber
+            style={{ width: 240 }}
+            defaultValue={100}
+            addonAfter="mA"
+          />
+        </Space>
+        <Space>
+          <InputNumber
+            size="small"
+            style={{ width: 240 }}
+            defaultValue={0}
+            addonAfter="mA"
+          />
+        </Space>
       </Space>
     </Space>
   );
