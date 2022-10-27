c162732971e92b77152952b5bdf6217597807566
zhangfuhao <>
2022-10-27 10:40:18 +0800

    feat: update badge demo

--- src/badge/demos/basic.tsx
+++ src/badge/demos/basic.tsx
@@ -6,28 +6,34 @@ const items = [
 ];
 
 export default () => {
+  const styles = {
+    lineHeight: "40px",
+    marginBottom: 16,
+    width: 100,
+  };
+
   return (
     <Space direction="vertical">
       <Space>
-        数值较小
+        <div style={styles}>数值较小</div>
         <Badge count={8}>
           <Tabs defaultActiveKey="item-1" type="card" items={items} />
         </Badge>
       </Space>
       <Space>
-        数值较大
+        <div style={styles}>数值较大</div>
         <Badge count={99}>
           <Tabs defaultActiveKey="item-1" type="card" items={items} />
         </Badge>
       </Space>
       <Space>
-        数字封顶
+        <div style={styles}>数字封顶</div>
         <Badge count={10000}>
           <Tabs defaultActiveKey="item-1" type="card" items={items} />
         </Badge>
       </Space>
       <Space>
-        自定义封顶数值
+        <div style={styles}>自定义封顶数值</div>
         <Badge overflowCount={500} count={10000}>
           <Tabs defaultActiveKey="item-1" type="card" items={items} />
         </Badge>
